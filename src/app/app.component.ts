a3194d996a66f9969b547fb7ab8603aebde3461f
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-11 11:08:52 +0100

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private httpClient : HttpClient) {
+  }
   title = 'client-for-Api';
+  users  : any;
+
+  ngOnInit(): void {
+    this.httpClient.get("https://localhost:5001/api/users").subscribe((data)=>{
+       this.users = data;
+       console.log(data);
+    })
+  }
 }
