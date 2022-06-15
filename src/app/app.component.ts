af953ff2feebbe0ea58a7566eaacc065f30e6b8e
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-15 16:28:30 +0100

    testing http get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(private httpClient : HttpClient) {
-  }
+
+export class AppComponent implements OnInit {
+
+  constructor(private httpClient : HttpClient ) {}
+
   title = 'client-for-Api';
   users  : any;
 
   ngOnInit(): void {
-    this.httpClient.get("https://localhost:5001/api/users").subscribe((data)=>{
-       this.users = data;
-       console.log(data);
-    })
+     this.httpClient.get("https://localhost:5001/api/users").subscribe((data)=>{
+        this.users = data;
+     })
   }
+
 }
